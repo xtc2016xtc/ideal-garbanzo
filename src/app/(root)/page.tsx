2b19051c24e083b9fae3aa566dea9a9788421d51
xtc2016xtc <728611827@qq.com>
2024-11-22 12:03:23 +0800

    布局调整

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button";
+
+const Home = () => {
+    return (
+        <Button>
+            首页
+        </Button>
+    )
+}
+
+export default Home;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,19 @@ import "./globals.css";
 import React from "react";
 
 const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+
+/*第二个字体*/
 const ibmPlexSerif = IBM_Plex_Serif({
   subsets: ['latin'],
   weight: ['400', '700'],
   variable: '--font-ibm-plex-serif'
 })
+
+/*元数据*/
 export const metadata: Metadata = {
   title: "银行交易",
   description: "银行交易.",
+  /*图标*/
   icons: {
     icon: '/icons/logo.svg'
   }
