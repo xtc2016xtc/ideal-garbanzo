d3c5488141520ed50aec160c2efc89522f82d18f
xtc2016xtc <728611827@qq.com>
2024-11-26 18:12:53 +0800

    类型渲染

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,13 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import {z} from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+export const authFormSchema = (type:string) => z.object({
+  /*注册*/
+  firstName: type === 'sign-in' ? z.string().optional():z.string().min(3),
+  lastName: type === 'sign-in' ? z.string().optional():z.string().min(3),
+})
