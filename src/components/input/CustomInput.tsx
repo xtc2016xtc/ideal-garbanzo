3103bd6b6e3cd35b0d725c4dd0b7f7287f2943d2
xtc2016xtc <728611827@qq.com>
2024-11-26 16:40:39 +0800

    分布式传递

---
+++ src/components/input/CustomInput.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+// import {z} from "zod";
+// import {Control} from "react-hook-form";
+
+
+// interface CustomInputProps {
+//     control:Control<z.infer<typeof formSchema>>
+// }
+
+
+const CustomInput:React.FC = ({  }) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CustomInput
